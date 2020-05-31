2295b049d5e8c1aab5cbefe7eda2f2947a229f59
Chris Wilding <hello@chriswilding.co.uk>
2020-05-31 15:31:09 +0100

    Initial commit

---
+++ src/test/java/dev/wilding/AppTest.java
@@ -0,0 +1,12 @@
+package dev.wilding;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
